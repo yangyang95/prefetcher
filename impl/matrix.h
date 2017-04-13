8daf2a2d8ef8567ad91304dc14a051a492f47aa2
yangyang95 <yangyang.weng@gmail.com>
2017-04-13 11:12:12 +0800

    Add matrix structure

---
+++ impl/matrix.h
@@ -0,0 +1,28 @@
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+#include <stdbool.h>
+
+/* predefined shortcut */
+#define DECLARE_MATRIX(row, col) \
+    typedef struct { float values[col][row]; } Mat ## row ## x ## col
+DECLARE_MATRIX(3, 3);
+DECLARE_MATRIX(4, 4);
+
+typedef struct {
+    int row, col;
+    void *priv;
+} Matrix;
+
+typedef struct {
+    Matrix* (*create)(int row, int col);
+    void (*assign)(Matrix *thiz, float* data, int data_size);
+    bool (*equal)(const Matrix *l, const Matrix *r);
+    void (*transpose)(Matrix *dst, Matrix *src);
+    void (*print)(Matrix *thiz);
+} MatrixAlgo;
+
+/* Available matrix providers */
+extern MatrixAlgo NaiveMatrixProvider;
+
+#endif /* MATRIX_H_ */
